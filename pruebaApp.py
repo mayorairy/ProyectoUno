27bc3e7fca082a94ac562cb354c3add6349214f7
mayorairy <123536708+mayorairy@users.noreply.github.com>
2025-01-14 11:21:39 -0600

    Update pruebaApp.py

--- pruebaApp.py
+++ pruebaApp.py
@@ -1,2 +1,8 @@
 import streamlit as st
 st.write ("Hola mundo")
+
+enable = st.checkbox("Enable camera")
+picture = st.camera_input("Take a picture", disabled=not enable)
+
+if picture:
+    st.image(picture)
