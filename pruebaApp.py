39bb9cb5971a4f7cfd0c6eca9586335fe6bb327c
mayorairy <123536708+mayorairy@users.noreply.github.com>
2025-01-14 12:15:44 -0600

    Update pruebaApp.py

--- pruebaApp.py
+++ pruebaApp.py
@@ -3,16 +3,6 @@ import time
 st.write ("Hola mundo")
 
 enable = st.checkbox("Activar camera")
-
-st.set_page_config()
-
-ph = st.empty()
-N = 5*60
-for secs in range(N,0,-1):
-    mm, ss = secs//60, secs%60
-    ph.metric("Countdown", f"{mm:02d}:{ss:02d}")
-    time.sleep(1)
-
 picture = st.camera_input("Take a picture", disabled=not enable)
 
 if picture:
