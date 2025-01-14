2d164309fc54e514d42271e04f06626f22d031ad
mayorairy <123536708+mayorairy@users.noreply.github.com>
2025-01-14 12:52:36 -0600

    Update pruebaApp.py

--- pruebaApp.py
+++ pruebaApp.py
@@ -5,15 +5,15 @@ st.write ("Hola mundo")
 enable = st.checkbox("Enable camera")
 picture = st.camera_input("Take a picture", disabled=not enable)
 
-#if picture:
-#    st.image(picture)
+if picture:
+    st.image(picture)
 
 if img_file_buffer is not None:
     # To read image file buffer as bytes:
     bytes_data = img_file_buffer.getvalue()
     # Check the type of bytes_data:
     # Should output: <class 'bytes'>
-       st.write(type(bytes_data))
+
 
 
 
