6fb7347d4fd94ea8e1fa03cdf0c3e79684a556a8
mayorairy <123536708+mayorairy@users.noreply.github.com>
2025-01-14 12:27:56 -0600

    Update pruebaApp.py

--- pruebaApp.py
+++ pruebaApp.py
@@ -1,18 +1,15 @@
 import streamlit as st
+import time
+
 st.write ("Hola mundo")
 
 enable = st.checkbox("Activar camera")
 
-import time
-
 ph = st.empty()
-N = 1*60
-for secs in range(N,0,-1):
+for secs in range(10,0,-1):
     mm, ss = secs//60, secs%60
     ph.metric("Countdown", f"{mm:02d}:{ss:02d}")
     time.sleep(1)
-    
-picture = st.camera_input("Take a picture", disabled=not enable)
 
 if enable:
     picture = st.camera_input("Toma una foto")
