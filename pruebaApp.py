c61f55ca2949547f357850f732bee5ebfce52a96
mayorairy <123536708+mayorairy@users.noreply.github.com>
2025-01-14 12:46:57 -0600

    Update pruebaApp.py

--- pruebaApp.py
+++ pruebaApp.py
@@ -4,28 +4,14 @@ import time
 st.write ("Hola mundo")
 
 enable = st.checkbox("Activar camera")
+img_file_buffer = st.camera_input("Take a picture")
 
-ph = st.empty()
-for secs in range(10,0,-1):
-    mm, ss = secs//60, secs%60
-    ph.metric("Countdown", f"{mm:02d}:{ss:02d}")
-    time.sleep(1)
-
-if enable:
-#    picture = st.camera_input("Toma una foto")
-#    if picture:
-#        st.image(picture)
-
-    img_file_buffer = st.camera_input("Take a picture", disabled= True)
-    
-
-    if img_file_buffer is not None:
-        # To read image file buffer as bytes:
-        bytes_data = img_file_buffer.getvalue()
+if img_file_buffer is not None:
+    # To read image file buffer as bytes:
+    bytes_data = img_file_buffer.getvalue()
     # Check the type of bytes_data:
     # Should output: <class 'bytes'>
-        st.write(type(bytes_data))
-
+    st.write(type(bytes_data))
 
 
 
