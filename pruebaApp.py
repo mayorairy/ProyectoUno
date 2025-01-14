acf4fe492eb413e3c8326733cad4cca334cbe5fd
mayorairy <123536708+mayorairy@users.noreply.github.com>
2025-01-14 12:16:26 -0600

    Update pruebaApp.py

--- pruebaApp.py
+++ pruebaApp.py
@@ -1,8 +1,18 @@
 import streamlit as st
-import time
 st.write ("Hola mundo")
 
 enable = st.checkbox("Activar camera")
+
+import time
+st.set_page_config()
+
+ph = st.empty()
+N = 5*60
+for secs in range(N,0,-1):
+    mm, ss = secs//60, secs%60
+    ph.metric("Countdown", f"{mm:02d}:{ss:02d}")
+    time.sleep(1)
+    
 picture = st.camera_input("Take a picture", disabled=not enable)
 
 if picture:
