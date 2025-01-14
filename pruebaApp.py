103f4f5bdf8db7c4776f2e7435b7b16c17b24238
mayorairy <123536708+mayorairy@users.noreply.github.com>
2025-01-14 12:48:24 -0600

    Update pruebaApp.py

--- pruebaApp.py
+++ pruebaApp.py
@@ -2,13 +2,15 @@ import streamlit as st
 import time
 
 st.write ("Hola mundo")
+enable = st.checkbox("Enable camera")
+picture = st.camera_input("Take a picture", disabled=not enable)
 
-enable = st.checkbox("Activar camera")
-img_file_buffer = st.camera_input("Take a picture")
+if picture:
+    st.image(picture)
 
-if img_file_buffer is not None:
+#if img_file_buffer is not None:
     # To read image file buffer as bytes:
-    bytes_data = img_file_buffer.getvalue()
+#    bytes_data = img_file_buffer.getvalue()
     # Check the type of bytes_data:
     # Should output: <class 'bytes'>
     st.write(type(bytes_data))
