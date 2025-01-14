5d633ff1f69a7f82160454e3026ac5ffa53a6b68
mayorairy <123536708+mayorairy@users.noreply.github.com>
2025-01-14 12:49:35 -0600

    Update pruebaApp.py

--- pruebaApp.py
+++ pruebaApp.py
@@ -10,10 +10,10 @@ if picture:
 
 #if img_file_buffer is not None:
     # To read image file buffer as bytes:
-#    bytes_data = img_file_buffer.getvalue()
+    bytes_data = img_file_buffer.getvalue()
     # Check the type of bytes_data:
     # Should output: <class 'bytes'>
-    st.write(type(bytes_data))
+   st.write(type(bytes_data))
 
 
 
