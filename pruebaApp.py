4d4244bc8778f073ae563e9fc4fec4e4ac6a41c1
mayorairy <123536708+mayorairy@users.noreply.github.com>
2025-01-14 12:43:57 -0600

    Update pruebaApp.py

--- pruebaApp.py
+++ pruebaApp.py
@@ -16,7 +16,8 @@ if enable:
 #    if picture:
 #        st.image(picture)
 
-    img_file_buffer = st.camera_input("Take a picture")
+    img_file_buffer = st.camera_input("Take a picture", disable= True)
+    
 
     if img_file_buffer is not None:
         # To read image file buffer as bytes:
