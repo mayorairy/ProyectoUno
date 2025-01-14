97e7a74640242717db742736a14fbe95d5e81d93
mayorairy <123536708+mayorairy@users.noreply.github.com>
2025-01-14 12:18:19 -0600

    Update pruebaApp.py

--- pruebaApp.py
+++ pruebaApp.py
@@ -6,7 +6,7 @@ enable = st.checkbox("Activar camera")
 import time
 
 ph = st.empty()
-N = 5*60
+N = 01*60
 for secs in range(N,0,-1):
     mm, ss = secs//60, secs%60
     ph.metric("Countdown", f"{mm:02d}:{ss:02d}")
