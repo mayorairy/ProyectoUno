4ebb333c57bd0455cfbc3893e87c1858d11c066c
mayorairy <123536708+mayorairy@users.noreply.github.com>
2025-01-21 10:52:21 -0600

    Update pruebaApp.py

--- pruebaApp.py
+++ pruebaApp.py
@@ -5,7 +5,8 @@ def pagina_principal():
     st.write("Home")
     st.title("Bienvenido!")
     st.write("Usa le menú de la izquierda para navegar entre las páginas!")
-    col1, col2, col3, col4 = st.columns(4)
+    
+col1, col2, col3, col4 = st.columns(4)
 
 with col1:
     st.subheader("¿Qúe es la discapacidad?")
