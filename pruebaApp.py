48b5c04c0cc8049584d9d55d79b3bf9d421cc832
mayorairy <123536708+mayorairy@users.noreply.github.com>
2025-01-20 19:50:10 -0600

    Update pruebaApp.py

--- pruebaApp.py
+++ pruebaApp.py
@@ -1,5 +1,14 @@
 import streamlit as st
 import time
+import streamlit as st
+
+st.header("_Streamlit_ is :blue[cool] :sunglasses:")
+st.header("This is a header with a divider", divider="gray")
+st.header("These headers have rotating dividers", divider=True)
+st.header("One", divider=True)
+st.header("Two", divider=True)
+st.header("Three", divider=True)
+st.header("Four", divider=True)
 
 st.write ("Hola mundo")
 enable = st.checkbox("Enable camera")
