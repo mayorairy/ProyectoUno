39ef0c681b54165a3afba0f74ecf6a1bfa88fbf9
mayorairy <123536708+mayorairy@users.noreply.github.com>
2025-01-23 15:34:15 -0600

    Update pruebaApp.py

--- pruebaApp.py
+++ pruebaApp.py
@@ -10,29 +10,29 @@ def pagina_principal():
  
     container.col1, container.col2, container.col3, container.col4 = st.columns(4)
         
-    with col1:
+    with container.col1:
         st.subheader("¿Qúe es la discapacidad?")
         st.image("https://i.pinimg.com/736x/cf/66/33/cf66334166ddd4c120148dc07c492449.jpg")
         st.link_button("Ver más", "https://streamlit.io/gallery")
         
         
-    with col2:
+    with container.col2:
         st.subheader("Academia de Lengua de Señas Mexicana")
         st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLPDBoTi6oPkFB26U2bZSFX5xrVgxGI72FAw&s")
         st.link_button("Ver más", "https://streamlit.io/gallery")
             
         
-    with col3:
+    with container.col3:
         st.subheader("Modelos y ayudas técnicas para la intervención")
         st.image("https://i.pinimg.com/736x/9d/3a/35/9d3a3531044579ff2381b7bcba1868fd.jpg")
         st.link_button("Ver más", "https://streamlit.io/gallery")
         
-    with col4:
+    with container.col4:
         st.subheader("Un bilingüismo silencioso")
         st.image("https://i.pinimg.com/736x/9f/ce/c2/9fcec281834ca98e08f1587e8943e983.jpg")
         st.link_button("Ver más", "https://streamlit.io/gallery")
         
-st.button("Comenzar a practicar LSM", type="primary")
+    container.button("Comenzar a practicar LSM", type="primary")
 
 def practicar_lsm():
     enable = st.checkbox("Enable camera")
